e99a92c5720535da614332a61b5dfe4171e9256f
cardosowellington <cardoso@Cardoso.>
2025-08-04 21:26:49 -0300

    created app courses

--- projetos/03/simplemooc/simplemooc/settings.py
+++ projetos/03/simplemooc/simplemooc/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'simplemooc.core.apps.CoreConfig',
+    'simplemooc.courses.apps.CoursesConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'simplemooc' / 'media'
