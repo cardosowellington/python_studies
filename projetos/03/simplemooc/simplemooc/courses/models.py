879cfd66b034fdcb263557b90663d1754775c9e1
cardosowellington <cardoso@Cardoso.>
2025-08-13 10:13:32 -0300

    custom manager

--- projetos/03/simplemooc/simplemooc/courses/models.py
+++ projetos/03/simplemooc/simplemooc/courses/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+class CourseManager(models.Manager):
+  def search(self, query):
+    return self.get_queryset().filter(
+      models.Q(name__icontains=query) | models.Q(description__icontains=query)
+    )
+
+
 class Course(models.Model):
   name = models.CharField('Nome', max_length=100)
   slug = models.SlugField('Atalho')
   description = models.TextField('Descrição, blank=True')
-  image = models.ImageField(upload_to='courses/images', verbose_name='Image')
+  image = models.ImageField(upload_to='courses/images', verbose_name='Image', null=True, blank=True)
   created_at = models.DateTimeField('Criado em', auto_now_add=True)
   updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+  objects = CourseManager()
